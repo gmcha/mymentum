d923caacce355c67f7b78040085415f1adb64735
Ga Min Cha <prettygamin@naver.com>
2023-01-04 12:46:23 +0900

    Update: login

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
-const h1 = document.querySelector(".hello h1")
+const loginForm = document.querySelector("#login-form")
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-function handleTitleClick(){
-    h1.classList.toggle("clicked");
+const HIDDEN_CLASSNAME = "hidden"; 
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
-h1.addEventListener("click", handleTitleClick);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}!`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+if(savedUsername === null){
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit)
+} else {
+    // show the greetings
+    paintGreetings(savedUsername);
+}
