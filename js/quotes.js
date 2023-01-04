68a76ae1c4195a54307f2c2226f43f5ce1a5d402
Ga Min Cha <prettygamin@naver.com>
2023-01-04 15:23:05 +0900

    Update: console.log quotes and author

--- js/quotes.js
+++ js/quotes.js
@@ -40,3 +40,11 @@ const quotes = [
         author: "Daniel Webster",
     },
 ]
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
