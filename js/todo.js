64cadfc95fd053ee247900a43172ccd67cda0e3f
Ga Min Cha <prettygamin@naver.com>
2023-01-06 16:24:22 +0900

    Update: #7.4 - loading toDos(2)

--- js/todo.js
+++ js/todo.js
@@ -6,7 +6,7 @@ const toDoList = document.getElementById("todo-list");
 
 const TODOS_KEY = "todos";
 
-const toDos = [];
+let toDos = [];
 
 function saveToDos(){
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
@@ -20,7 +20,6 @@ function deleteTodo(event){
 function paintToDo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
-    span.innerText = newTodo; 
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", deleteTodo);
@@ -41,7 +40,9 @@ function handleToDoSubmit(event){
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
+
 if(savedToDos != null){
     const parsedToDos = JSON.parse(savedToDos);
-    parsedToDos.forEach((item) => console.log("this is the turn of ", item));
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
 }
