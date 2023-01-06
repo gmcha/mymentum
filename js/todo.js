d7b4bde5d0b0a29f09b703a398d536508472fbf5
Ga Min Cha <prettygamin@naver.com>
2023-01-06 14:58:08 +0900

    Update: #7.2 - deleting li from a todo list

--- js/todo.js
+++ js/todo.js
@@ -1,22 +1,31 @@
-// todo를 입력하는 부분 구현
+// todo를 지울 button 생성 ("click" event를 기다릴거임)
 
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteTodo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span); // span을 li 안에 넣음
-    span.innerText = newTodo; // span안에 todo 넣기
-    toDoList.appendChild(li); // ul에 li 넣기
+    span.innerText = newTodo; 
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteTodo);
+    li.appendChild(span); 
+    li.appendChild(button);
+    toDoList.appendChild(li); 
 }
 
 function handleToDoSubmit(event){
     event.preventDefault(); 
     const newTodo = toDoInput.value; 
     toDoInput.value = ""; 
-    paintToDo(newTodo); // todo를 입력시킬 함수 실행
+    paintToDo(newTodo); 
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
